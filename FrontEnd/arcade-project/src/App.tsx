50813b0a1a6233473cfb2470ee689b4f15860dec
zyam1 <zyam8441@gmail.com>
2024-01-16 16:23:32 +0900

    프론트 파일 생성

---
+++ FrontEnd/arcade-project/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Test from './components/Test';
+
+function App() {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd/arcade-project/src/components/Test.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div>아케이드 페이지 태스트</div>;
+};
+
+export default Test;
